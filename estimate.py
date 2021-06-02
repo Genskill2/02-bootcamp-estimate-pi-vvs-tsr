1212f1077f70437356fb5cb224cf6ea9b8256533
vvs-tsr <vvs.tsr@gmail.com>
2021-06-02 16:00:34 +0530

    test 3

--- estimate.py
+++ estimate.py
@@ -6,14 +6,14 @@ def wallis(m):
   return x
    
   
- def monte_carlo(m):
-  total_no_of_points=0
-  points_inside_circle=0
-  for i in range (0,m):
-    x=random.random()
-    y=random.random()
-    total_no_of_points+=1
-    if ((x*x)+(y*y))<=1:
-      points_inside_circle+=1
-  return 4*(points_inside_circle)/(total_no_of_points)
+def monte_carlo(m):
+ total_no_of_points=0
+ points_inside_circle=0
+ for i in range (0,m):
+   x=random.random()
+   y=random.random()
+   total_no_of_points+=1
+   if ((x*x)+(y*y))<=1:
+     points_inside_circle+=1
+ return 4*(points_inside_circle)/(total_no_of_points)
  
