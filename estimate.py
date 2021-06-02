ec1fb5d382ab0f387b4e415f0516a27c22209ccd
vvs-tsr <vvs.tsr@gmail.com>
2021-06-02 16:09:51 +0530

    test 4

--- estimate.py
+++ estimate.py
@@ -1,9 +1,9 @@
 def wallis(m):
-  x=2
-  for k in range(1,1+m):
-    x=x*(4*k*k)/((4*k*k)-1)
-    
-  return x
+  pr=1
+  for k in range(1,m+1):
+    x=(4*k*k)/((4*k*k)-1)
+    pr=pr*x
+  return 2*x
    
   
 def monte_carlo(m):
@@ -15,5 +15,5 @@ def monte_carlo(m):
    total_no_of_points+=1
    if ((x*x)+(y*y))<=1:
      points_inside_circle+=1
- return 4*(points_inside_circle)/(total_no_of_points)
+ return 4*((points_inside_circle)/(total_no_of_points))
  
